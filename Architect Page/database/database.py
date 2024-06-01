871a78097741234aa5e1cff39a116c1df0b2f842
ff4L08 <109841553+ff4L08@users.noreply.github.com>
2024-06-01 05:01:03 +0000

    Add files via upload

--- Architect Page/database/database.py
+++ Architect Page/database/database.py
@@ -9,16 +9,16 @@ query = 'CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY NOT NULL, imag
 cursor.execute(query)
 
 #add images
-query = '''INSERT INTO images VALUES (001, "https://i.imgur.com/v9YSDnn.jpeg"),
-(002, "https://i.imgur.com/aEtoU3E.jpeg"),
-(003, "https://i.imgur.com/FLZuyNC.jpeg"),
-(004, "https://i.imgur.com/avI1Q1O.jpeg"),
-(005, "https://i.imgur.com/XxYxOOQ.png"),
-(006, "https://i.imgur.com/CGZOd.jpeg"),
-(007, "https://i.imgur.com/EeSppj0.gif"),
-(008, "https://i.imgur.com/ht7Drzv.gif"),
-(009, "https://i.imgur.com/kAo2LG8.gif"),
-(010, "https://i.imgur.com/kAo2LG8.gif")
+query = '''INSERT INTO images VALUES (1, "https://i.imgur.com/v9YSDnn.jpeg"),
+(2, "https://i.imgur.com/aEtoU3E.jpeg"),
+(3, "https://i.imgur.com/FLZuyNC.jpeg"),
+(4, "https://i.imgur.com/avI1Q1O.jpeg"),
+(5, "https://i.imgur.com/XxYxOOQ.png"),
+(6, "https://i.imgur.com/CGZOd.jpeg"),
+(7, "https://i.imgur.com/EeSppj0.gif"),
+(8, "https://i.imgur.com/ht7Drzv.gif"),
+(9, "https://i.imgur.com/kAo2LG8.gif"),
+(10, "https://i.imgur.com/kAo2LG8.gif")
 '''
 cursor.execute(query)
 
